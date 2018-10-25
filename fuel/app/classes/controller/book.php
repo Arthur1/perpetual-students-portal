35b537cdac9ae33ccdb32d4a97730637eb98a777
Kazuki Asakura <32762324+arthur-teleneco@users.noreply.github.com>
2018-10-25 23:44:13 +0900

    img -> image

--- fuel/app/classes/controller/book.php
+++ fuel/app/classes/controller/book.php
@@ -4,7 +4,7 @@ class Controller_Book extends Controller_template
 	public function action_index()
 	{
 		$this->template->title = '教養特論：アグリコラ拡張学・特設ページ';
-		$this->template->ogp_img_large = 'book/banner.png';
+		$this->template->$ogp_image_large = 'book/banner.png';
 		$this->template->description = '2018年11月下旬より、アグリコラ拡張本「教養特論：アグリコラ拡張学」を頒布いたします！';
 		$this->template->contents = View::forge('book/index');
 	}
