ee5a4551c6d3334521b086db2aba2628c502fa14
Kazuki Asakura <32762324+arthur-teleneco@users.noreply.github.com>
2018-11-17 06:07:43 +0900

    特設ページ修正

--- fuel/app/views/book/index.php
+++ fuel/app/views/book/index.php
@@ -2,13 +2,13 @@
 <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 <?= Html::anchor(Uri::create('https://twitter.com/share', [], ['url' => Uri::current(), 'hashtags' => 'アグリコラ拡張本']), 'Tweet', ['class' => 'twitter-share-button', 'data-show-count' => 'false']); ?>
 <p>
-	ぶらつき学生連盟が2018年11月下旬より頒布するアグリコラ拡張本「教養特論：アグリコラ拡張学」の情報をお届けします。
+	ぶらつき学生連盟が2018年11月21日より頒布するアグリコラ拡張本「教養特論：アグリコラ拡張学」の情報をお届けします。
 </p>
 <div class="row">
 	<div class="col s12 m8">
 		<h2 class="green-text text-darken-1">アグリコラ拡張本って？</h2>
 		<p>
-			アグリコラには、基本のEIKデッキ以外にも数多くの拡張デッキが発売されています。私たち「ぶらつき学生連盟」では、拡張カードを使用したアグリコラをプレイし、その結果を統計として本サイト「ぶらつき学生ポータル」に残しています。そんなデータと数多くのプレイ経験から生まれたのがこのアグリコラ拡張本。全100ページのそこそこなボリューム。500円という制作費ギリギリの頒価。拡張未経験者から経験者まで幅広くターゲットにした内容になっています。
+			アグリコラには、基本のEIKデッキ以外にも数多くの拡張デッキが発売されています。私たち「ぶらつき学生連盟」では、拡張カードを使用したアグリコラをプレイし、その結果を統計として本サイト「ぶらつき学生ポータル」に残しています。そんなデータと数多くのプレイ経験から生まれたのがこのアグリコラ拡張本。全94ページのそこそこなボリューム。500円という制作費ギリギリの頒価。拡張未経験者から経験者まで幅広くターゲットにした内容になっています。
 		</p>
 	</div>
 	<div class="col s12 m4">
@@ -19,7 +19,7 @@
 <h2 class="green-text text-darken-1">頒布情報</h2>
 <h3 class="yellow-text text-darken-2">ゲームマーケット</h3>
 <p>
-	ゲームマーケット2018秋の2日目にピンゾロゲームスのブースにて頒布いたします。ピンゾロゲームスでは、旧作の「収束判定ゲーム」「Thunder Chase」「協同開発」に加えて新作ゲームも頒布しますので、こちらもぜひお求めください。
+	ゲームマーケット2018秋の2日目にピンゾロゲームスのブースにて頒布いたします。ピンゾロゲームスでは、旧作の「収束判定ゲーム」「Thunder Chase」「協同開発」も頒布しますので、こちらもぜひお求めください。少部数のご用意となりますので、確実にゲームマーケットにて手に入れたい方はご予約をおすすめします。
 </p>
 <dl>
 	<dt class="green-text text-darken-1">日時</dt>
@@ -28,10 +28,12 @@
 	<dd>東京ビッグサイト</dd>
 	<dt class="green-text text-darken-1">サークル名</dt>
 	<dd>ピンゾロゲームス</dd>
-	<dt class="green-text text-darken-1">サークル番号</dt>
-	<dd>未定</dd>
+	<dt class="green-text text-darken-1">ブース番号</dt>
+	<dd>2018秋 日-E60</dd>
 	<dt class="green-text text-darken-1">頒価</dt>
-	<dd>500円(予定)</dd>
+	<dd>500円</dd>
+	<dt class="green-text text-darken-1">予約</dt>
+	<dd><a href="https://docs.google.com/forms/d/e/1FAIpQLSd2h7vOle995i2hcMN0cBzy5556FD-aH52LaQgdlfyR4q1KMg/viewform?usp=sf_link" target="_blank">予約フォーム</a>をご利用ください。</dd>
 </dl>
 <h3 class="yellow-text text-darken-2">通販</h3>
 <p>
@@ -63,6 +65,15 @@
 		<?= Asset::img('book/prev3.jpg', ['alt' => '見本3', 'class' => 'responsive-img']); ?>
 	</div>
 </div>
+<h2 class="green-text text-darken-1">Q&A</h2>
+<h3 class="light-green-text text-darken-1">Q.ゲームマーケット当日は製作者に会えますか？</h3>
+<p>
+	残念ながらArthurは2日間とも用事があるので、頒布はピンゾロゲームス(日-E60)に委託しております。もしご用がございましたら、Twitter(<a href="https://twitter.com/aaron_teleneco" target="_blank">@aaron_teleneco</a>)までご連絡いただくか、一橋大学国立キャンパスにお越しください。
+</p>
+<h3 class="light-green-text text-darken-1">Q.電子書籍での頒布はありますか？</h3>
+<p>
+	A.それなりの需要があるのならお応えしたいところですが、電子書籍に関する知識が全く無いので……すぐの対応は厳しいです。でも頑張ります。
+</p>
 <h2 class="green-text text-darken-1">連絡先</h2>
 <p>
 	質問などございましたら、Twitterの<a href="https://twitter.com/aaron_teleneco" target="_blank">@aaron_teleneco</a>までお気軽にどうぞ。
